cc01ab932bee5572eb8e2e536c5e6d3528ec64ba
ozan <ozan_isik14@hotmail.com>
2020-07-29 22:31:27 -0400

    added if picked square is correct or not. Added display of correct RGB

--- colorGame.js
+++ colorGame.js
@@ -6,9 +6,24 @@ var colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)",
 ]
-
+var pickedColor =  colors[3];
 var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener("click", function(){
+        var clickedColor = this.style.backgroundColor;
+
+        if(clickedColor == pickedColor){
+            messageDisplay.textContent = "Correct";
+        }else{
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "try again";
+        }
+    })
 }
