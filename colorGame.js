a111ea8d8573846861ce9e9025e33fd7c67a8f88
ozan <ozan_isik14@hotmail.com>
2020-08-03 16:16:44 -0400

    background of h1 changes to the correct color if picked correct

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 var pickedColor =  pickColor();
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,6 +16,7 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor == pickedColor){
             messageDisplay.textContent = "Correct";
             changeColor(pickedColor);
+            h1.style.background = clickedColor;
         }else{
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "try again";
@@ -40,4 +35,22 @@ function pickColor(){
    var random = Math.floor(Math.random() * colors.length) + 1;
    return colors[random];
 }
-console.log(pickColor());
+
+function generateRandomColors(num){
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+    
+
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 255 + 1);
+    var g = Math.floor(Math.random() * 255 + 1);
+    var b = Math.floor(Math.random() * 255 + 1);
+
+    return "rgb("+r+", "+ g+", " +b+")";
+
+}
