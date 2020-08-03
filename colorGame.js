512ec6888d1084aa52bf1c2a5a46211f7982a922
ozan <ozan_isik14@hotmail.com>
2020-08-03 15:44:32 -0400

    added randomized colors with changeColor and pickColor functions

--- colorGame.js
+++ colorGame.js
@@ -6,7 +6,7 @@ var colors = [
     "rgb(0, 0, 255)",
     "rgb(255, 0, 255)",
 ]
-var pickedColor =  colors[3];
+var pickedColor =  pickColor();
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
@@ -21,9 +21,23 @@ for(var i = 0; i < squares.length; i++){
 
         if(clickedColor == pickedColor){
             messageDisplay.textContent = "Correct";
+            changeColor(pickedColor);
         }else{
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "try again";
         }
     })
 }
+
+function changeColor(color){
+    for(var i=0; i<squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+
+}
+
+function pickColor(){
+   var random = Math.floor(Math.random() * colors.length) + 1;
+   return colors[random];
+}
+console.log(pickColor());
